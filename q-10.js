fd973a0033ec4c7dda7ed683c9b4f7a705ab6db0
mehulrajdev <mehulrajpnbe@gmail.com>
2023-09-06 22:32:20 +0530

    added  q-11.js

--- q-10.js
+++ q-10.js
@@ -2,3 +2,20 @@
 Array.prototype.last = function () {
   return 0 === this.length ? -1 : this[this.length - 1];
 };
+
+
+// /**
+//  * @param {number} millis
+//  */
+// async function sleep(millis) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, millis)
+//     });
+// }
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
