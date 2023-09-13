be391b153a53bfcb8d42ec7cfdd9fa9b95d4d179
mehulrajdev <mehulrajpnbe@gmail.com>
2023-09-13 16:20:39 +0530

    update  q-12.js

--- q-12.js
+++ q-12.js
@@ -1,1 +1,16 @@
 // 2626. Array Reduce Transformation
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let res = init;
+
+  for (const i of nums) {
+    res = fn(res, i);
+  }
+
+  return res;
+};
